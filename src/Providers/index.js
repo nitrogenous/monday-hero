aca322bb7f9f7c4f875789577af5f67f3662a609
Toprak Koç <hello@toprak.dev>
2020-09-20 04:13:34 +0300

    Removed storage provider

--- src/Providers/index.js
+++ src/Providers/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { StorageProvider } from "./StorageProvider";
 import { ProjectsProvider } from "./ProjectsProvider";
 
 
 export const Providers = ({ children }) => {
-    return <StorageProvider> 
-        <ProjectsProvider> { children } </ProjectsProvider>
-    </StorageProvider>;
+    return  <ProjectsProvider> { children } </ProjectsProvider>;
 }
