7c51b43158cc428186800b37eae3ae26240ef4bf
Toprak Koç <hello@toprak.dev>
2020-09-20 23:51:37 +0300

    created list item properties and added getIconOfProjectType function

--- src/Components/ListItem.tsx
+++ src/Components/ListItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Project } from '../Providers/ProjectsProvider';
 import { Card } from 'antd';
+import { AppleOutlined, AndroidOutlined } from '@ant-design/icons';
 
 interface Props {
     project: Project,
@@ -8,10 +9,29 @@ interface Props {
 }
 
 const ListItem = ({project, projectIndex}:Props) => {
+
+    const getIconOfProjectType = (projectType:string) => {
+        switch(projectType) {
+            case 'iOS': 
+                return (<AppleOutlined className='list-item project-logo'/>);
+            case 'Android':
+                return (<AndroidOutlined className='list-item project-logo'/>);
+            default:
+                return (<span>nothing</span>);
+        }
+    };
+
     return (
-        <Card title={project.projectName} style={{ width: 300 }} >
-            <p>test</p>
-        </Card>
+        <div className='list-item card'>
+            <span>{project.projectName}</span>
+            <div className='list-item project-type-wrapper'>
+                {getIconOfProjectType(project.projectType)}
+                {/* <AppleOutlined className='list-item project-logo'/> */}
+                {/* <AndroidOutlined className='list-item project-logo'/> */}
+                <span className='list-item project-type'>{project.projectType}</span>
+            </div>
+            <p>{new Date(project.projectCreateDate).toString()}</p>
+        </div>
     );
 };
 
