00de0abda53da194fa86e9ec7e07f5df9eca67eb
Toprak Koç <hello@toprak.dev>
2020-09-21 17:22:48 +0300

    created create modal project component

---
+++ src/Components/CreateProjectModal.tsx
@@ -0,0 +1,66 @@
+import { Radio, Space } from 'antd';
+import React, { useContext, useState } from 'react';
+import { ProjectsContext } from '../Providers/ProjectsProvider';
+import Modal from 'antd/lib/modal/Modal';
+import { AppleOutlined, AndroidOutlined } from '@ant-design/icons';
+import { RadioChangeEvent } from 'antd/lib/radio';
+
+interface Props {
+    visibilityOfModal:boolean,
+    setVisibilityOfModal:(value: boolean) => void
+}
+
+const CreateProjectModal = ({visibilityOfModal, setVisibilityOfModal}:Props) => {
+    
+    var { createProject } = useContext(ProjectsContext);
+    const [selectedProjectType, setSelectedProjectType] = useState('iOS');
+    const [projectName, setProjectName] = useState(String);
+
+    const projectTypeOnChange = (event: RadioChangeEvent) => {
+        setSelectedProjectType(event.target.value);
+    };
+
+    const projectNameOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setProjectName(event.target.value)
+    }
+    
+    const createNewProject = () => {
+        const projectCreateDate = Date.now();
+        createProject({
+            'projectName': projectName,
+            'projectType': selectedProjectType,
+            'projectCreateDate': projectCreateDate
+        });
+        
+        setProjectName('');
+        setVisibilityOfModal(false);
+    };
+
+    return (
+        <Modal
+            className='projects create-project-modal'
+            visible={visibilityOfModal}
+            onOk={() => createNewProject()}
+            onCancel={() => setVisibilityOfModal(false)}
+            closable={false}>
+                <div className='projects create-project-modal content'>
+                    <Radio.Group id='project-type' defaultValue="iOS" buttonStyle="solid" onChange={projectTypeOnChange}>
+                        <Space size='middle'>
+                            <Radio.Button value="iOS" className='projects project-type-wrapper'>
+                                <AppleOutlined />
+                            </Radio.Button>
+                            <Radio.Button value="Android" className='projects project-type-wrapper'>
+                                <AndroidOutlined />
+                            </Radio.Button>
+                        </Space>
+                    </Radio.Group>
+                    <div  className='projects create-project-modal input-wrapper'>
+                        <input className='projects create-project-modal inputs' disabled placeholder='Wow you are in private beta!' />
+                        <input className='projects create-project-modal inputs' style={{background: 'none'}} placeholder='Project Name' value={projectName} onChange={projectNameOnChange} />
+                    </div>
+                </div>
+        </Modal>
+    );
+};
+
+export default CreateProjectModal;
--- src/Containers/Projects.tsx
+++ src/Containers/Projects.tsx
@@ -1,53 +1,23 @@
 import React, { useContext, useState } from 'react';
-import List from '../Components/List';
-import { Layout, Button, Typography, Modal, Radio, Space } from 'antd';
 import { ProjectsContext } from '../Providers/ProjectsProvider';
-import { AppleOutlined, AndroidOutlined, PlusOutlined } from '@ant-design/icons';
+import { Layout, Button, Typography } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import List from '../Components/List';
+import CreateProjectModal from '../Components/CreateProjectModal';
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
 
 const Projects = () => {
-    var { projectList, createProject } = useContext(ProjectsContext);
+    var { projectList } = useContext(ProjectsContext);
     const [visibilityOfModal, setVisibilityOfModal] = useState(Boolean);
-    const createProjectModal = () => {
-        return (
-            <Modal
-                className='projects create-project-modal'
-                visible={visibilityOfModal}
-                onOk={() => createNewProject()}
-                onCancel={() => setVisibilityOfModal(false)}
-                closable={false}>
-                    <div className='projects create-project-modal content'>
-                        <Radio.Group id='project-type' defaultValue="iOS" buttonStyle="solid">
-                            <Space size='middle'>
-                                <Radio.Button value="iOS" className='projects project-type-wrapper'>
-                                    <AppleOutlined />
-                                </Radio.Button>
-                                <Radio.Button value="Android" className='projects project-type-wrapper'>
-                                    <AndroidOutlined />
-                                </Radio.Button>
-                            </Space>
-                        </Radio.Group>
-                        <div  className='projects create-project-modal input-wrapper'>
-                            <input className='projects create-project-modal inputs' disabled placeholder='Wow you are in private beta!' />
-                            <input className='projects create-project-modal inputs' style={{background: 'none'}}placeholder='Project Name' />
-                        </div>
-                    </div>
-            </Modal>
-        );
-    };
-
-    const createNewProject = () => {
-        setVisibilityOfModal(false);
-    };
 
     return (
         <Layout className='projects layout' >
-            {createProjectModal()}
             <Header className='projects header'>
                 <Title level={2} className='projects title'>Projects</Title>
-                <Button className='projects button' icon={<PlusOutlined />}  onClick={() => {setVisibilityOfModal(true)}} />
+                <Button className='projects button' icon={<PlusOutlined />}  onClick={() => setVisibilityOfModal(true)} />
+                <CreateProjectModal visibilityOfModal={visibilityOfModal} setVisibilityOfModal={setVisibilityOfModal} />
             </Header>   
             <Content className='projects content'>
                 <div className='projects list-selector'>
@@ -55,8 +25,7 @@ const Projects = () => {
                     <Button size='small'>Templates</Button>
                 </div>
                 <List projectList={projectList} />
-            </Content> 
-          
+            </Content>
         </Layout>
     );
 };
