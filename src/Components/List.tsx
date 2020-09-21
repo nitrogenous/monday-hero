9a7cffd9a568ee40bccc0b64afa215625e205762
Toprak Koç <hello@toprak.dev>
2020-09-21 20:15:56 +0300

    added onitemclick and refactored

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Project } from '../Providers/ProjectsProvider';
 import ListItem from './ListItem';
 
 interface Props {
-    projectList:Object
+    projectList: Object
+    onItemClick: (item: Project) => void;
 }
 
-const List = ({projectList}:Props) => {
+const List = ({ projectList, onItemClick }: Props) => {
     return (
-        <div className='list-items-wrapper'> 
-            {Object.values(projectList).map((data, index) => { 
-                return (
-                    <ListItem project={data} projectIndex={index} key={index.toString()}/>
-                )  
-            })}
+        <div className='list-items-wrapper'>
+            {Object.values(projectList).map((data, index) => (
+                <ListItem project={data} key={data.id} onClick={() => {
+                    onItemClick(data);
+                }} />
+            ))
+            }
         </div>
     );
 };
