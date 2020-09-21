4b6f09a12fc3a0dcdb6c7ffcee597cb12cf0d97b
Toprak Koç <hello@toprak.dev>
2020-09-21 04:24:17 +0300

    added create project modal

--- src/Containers/Projects.tsx
+++ src/Containers/Projects.tsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import List from '../Components/List';
-import { Layout, Button, Typography } from 'antd';
+import { Layout, Button, Typography, Modal, Radio } from 'antd';
 import { ProjectsContext } from '../Providers/ProjectsProvider';
 import { PlusOutlined } from '@ant-design/icons';
 
@@ -9,12 +9,31 @@ const { Title } = Typography;
 
 const Projects = () => {
     var { projectList, createProject } = useContext(ProjectsContext);
-    console.log(projectList)
+    const [visibilityOfModal, setVisibilityOfModal] = useState(Boolean);
+
+    const createProjectModal = () => {
+        return (
+            <Modal
+                className='projects create-modal'
+                visible={visibilityOfModal}
+                onOk={() => setVisibilityOfModal(false)}
+                onCancel={() => setVisibilityOfModal(false)}
+                closable={false}>
+                <Radio.Group defaultValue="a" buttonStyle="solid">
+                    <Radio.Button value="a">Hangzhou</Radio.Button>
+                    <Radio.Button value="b">Shanghai</Radio.Button>
+                </Radio.Group>
+                <input />
+            </Modal>
+        );
+    }
+
     return (
         <Layout className='projects layout' >
+            {createProjectModal()}
             <Header className='projects header'>
                 <Title level={2} className='projects title'>Projects</Title>
-                <Button className='projects button' icon={<PlusOutlined />}  onClick={() => {createProject({projectName: 'mahmut', projectType: 'IOS', projectCreateDate: Date.now() })}} />
+                <Button className='projects button' icon={<PlusOutlined />}  onClick={() => {setVisibilityOfModal(true)}} />
             </Header>   
             <Content className='projects content'>
                 <div className='projects list-selector'>
