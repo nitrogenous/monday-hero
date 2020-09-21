abb82174baa56d745a74b40f0196f6678c8d97f6
Toprak Koç <hello@toprak.dev>
2020-09-21 20:15:15 +0300

    updated create project modal to project modal and visibility settings

--- src/Containers/Projects.tsx
+++ src/Containers/Projects.tsx
@@ -1,30 +1,37 @@
 import React, { useContext, useState } from 'react';
-import { ProjectsContext } from '../Providers/ProjectsProvider';
+import { Project, ProjectsContext } from '../Providers/ProjectsProvider';
 import { Layout, Button, Typography } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import List from '../Components/List';
-import CreateProjectModal from '../Components/CreateProjectModal';
+import ProjectModal from '../Components/ProjectModal';
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
 
 const Projects = () => {
-    var { projectList } = useContext(ProjectsContext);
-    const [visibilityOfModal, setVisibilityOfModal] = useState(Boolean);
+    const { projectList } = useContext(ProjectsContext);
+    const [editItem, setEditItem] = useState<Project | undefined>();
+    const [visibilityOfModal, setVisibilityOfModal] = useState(false);
 
     return (
         <Layout className='projects layout' >
             <Header className='projects header'>
                 <Title level={2} className='projects title'>Projects</Title>
-                <Button className='projects button' icon={<PlusOutlined />}  onClick={() => setVisibilityOfModal(true)} />
-                <CreateProjectModal visibilityOfModal={visibilityOfModal} setVisibilityOfModal={setVisibilityOfModal} />
-            </Header>   
+                <Button className='projects button' icon={<PlusOutlined />} onClick={() => setVisibilityOfModal(true)} />
+                {visibilityOfModal && <ProjectModal onClose={() => {
+                    setVisibilityOfModal(false);
+                    setEditItem(undefined);
+                }} editItem={editItem} />}
+            </Header>
             <Content className='projects content'>
                 <div className='projects list-selector'>
                     <Button type='primary' size='small'>My Projects</Button>
                     <Button size='small'>Templates</Button>
                 </div>
-                <List projectList={projectList} />
+                <List projectList={projectList} onItemClick={(item) => {
+                    setEditItem(item);
+                    setVisibilityOfModal(true)
+                }} />
             </Content>
         </Layout>
     );
