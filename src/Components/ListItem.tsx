0453e1b7c2fdbe25b155679f4607a4bb1a68f3d9
Toprak Koç <hello@toprak.dev>
2020-09-21 03:52:38 +0300

    created popup menu and added remove function

--- src/Components/ListItem.tsx
+++ src/Components/ListItem.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
-import { Project } from '../Providers/ProjectsProvider';
+import React, { useState, useContext } from 'react';
+import { Project, ProjectsContext } from '../Providers/ProjectsProvider';
 import { AppleOutlined, AndroidOutlined, MenuOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
 interface Props {
-    project: Project,
-    projectIndex: Number
+    project:Project,
+    projectIndex:number
 }
 
 const ListItem = ({project, projectIndex}:Props) => {
 
-    const [isHovering, setIsHovering] = useState(Boolean);
+    var { removeProject } = useContext(ProjectsContext);
+    const [showMenuButton, setShowMenuButton] = useState(Boolean);
+    const [showMenu, setShowMenu] = useState(Boolean);
 
     const getIconOfProjectType = (projectType:string) => {
         switch(projectType) {
@@ -22,17 +25,32 @@ const ListItem = ({project, projectIndex}:Props) => {
         }
     };
 
+    const popupMenu = () => {
+        return (
+            <div className='list-item popup-menu' onMouseLeave={() => {setShowMenu(false)}}>
+                <Button type='link'>
+                    Member Settings
+                </Button>
+                <Button type='link' onClick={() => {removeProject(projectIndex)}}>
+                    Remove Project
+                </Button>
+            </div>
+        )
+    };
+
     const betaMarkElement = <span className='list-item beta-mark'>BETA</span>;
+    const menuButton = <MenuOutlined className='list-item project-settings' onClick={() => {setShowMenu(true)}}/>;
 
     return (
         <div className='list-item card'>
             <span>{project.projectName}</span>
             <div 
                 className='list-item project-type-wrapper' 
-                onMouseEnter={() => {setIsHovering(true)}}
-                onMouseLeave={() => {setIsHovering(false)}}>
-                {isHovering && <MenuOutlined className='list-item project-settings'/>}
+                onMouseEnter={() => {setShowMenuButton(true)}}
+                onMouseLeave={() => {setShowMenuButton(false)}}>
                 {project.projectType === 'Android' && betaMarkElement}
+                {showMenuButton && menuButton}
+                {showMenu && popupMenu()}
                 {getIconOfProjectType(project.projectType)}
                 <span className='list-item project-type'>{project.projectType}</span>
             </div>
